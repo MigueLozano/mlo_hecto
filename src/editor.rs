a784ecd3a95afc3455e7d884b19d1f1c708cc1e7
Jose Miguel Lozano <jmlozano98@gmail.com>
2023-03-15 21:56:34 +0100

    Fixed flickering bug and how lines are cleared

--- src/editor.rs
+++ src/editor.rs
@@ -31,13 +31,15 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        Terminal::clear_screen();
+        Terminal::cursor_hide();
         Terminal::cursor_position(0, 0);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
         }
+        Terminal::cursor_show();
         Terminal::flush()
     }
 
@@ -51,7 +53,8 @@ impl Editor {
     }
     
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
+        for _ in 0..self.terminal.size().height - 1 {
+            Terminal::clear_current_line();
             println!("~\r");
         }
     }
--- src/terminal.rs
+++ src/terminal.rs
@@ -32,6 +32,9 @@ impl Terminal {
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
 
     pub fn cursor_position(x: u16, y: u16) {
         let x = x.saturating_add(1);
@@ -49,5 +52,12 @@ impl Terminal {
                 return key;
             }
         }
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
     }
 }
