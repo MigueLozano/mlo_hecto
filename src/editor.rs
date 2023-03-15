7ec4fd53bcac91f0d9225e1987e48a4588d4704f
Jose Miguel Lozano <jmlozano98@gmail.com>
2023-03-15 21:46:49 +0100

    Set up terminal object and initial view

---
+++ src/editor.rs
@@ -0,0 +1,63 @@
+use termion::event::Key;
+
+use crate::Terminal;
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {
+            should_quit: false,
+            terminal: Terminal::default()
+                .expect("Failed to initialize terminal"),
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
+        if self.should_quit {
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+        }
+        Terminal::flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+    
+    fn draw_rows(&self) {
+        for _ in 0..self.terminal.size().height {
+            println!("~\r");
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    Terminal::clear_screen();
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-use std::io::{self, Read, stdout};
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+mod terminal;
 
-use termion::raw::IntoRawMode;
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+use editor::Editor;
+pub use terminal::Terminal;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            },
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
