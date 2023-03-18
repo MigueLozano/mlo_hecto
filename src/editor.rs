edd1960ad69d81f473d4516b0e1b637e48519621
Jose Miguel Lozano <jmlozano98@gmail.com>
2023-03-18 20:09:44 +0100

    Implemented basic text input

--- src/editor.rs
+++ src/editor.rs
@@ -100,6 +100,15 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Char(c) => {
+                self.document.insert(&self.cursor_position, c);
+                self.move_cursor(Key::Right);
+            },
+            Key::Delete => self.document.delete(&self.cursor_position),
+            Key::Backspace => {
+                self.move_cursor(Key::Left);
+                self.document.delete(&self.cursor_position);
+            }
             Key::Up
             | Key::Down
             | Key::Left
